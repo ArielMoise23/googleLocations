af240e3c8d44af17e24f4455efcdc290849dbd01
ArielMoise23 <ariel.m23@gmail.com>
2024-06-25 12:01:49 -0300

    initial code

---
+++ locations.py
@@ -0,0 +1,37 @@
+import json
+from geopy.geocoders import Nominatim
+from utils import *
+
+# Define the file name
+file_name = '../Records.json'
+
+# Open the JSON file and load its content
+# will be changed to using google api and get the data for the user
+with open(file_name, 'r') as file:
+    data = json.load(file)
+
+# # Initialize the Nominatim geocoder
+geolocator = Nominatim(user_agent="00")
+
+locations: list = []
+coordinates: list = []
+
+# organize data
+for location in data['locations']:
+    # from google we get coordinates of E7. we need to convert to the typical cordinates.
+    latitude: int = int(location['latitudeE7']) / 10**7
+    longitude: int = int(location['longitudeE7']) / 10**7
+
+    time = parse_timestamp(location['timestamp'])
+    locations.append([time, (latitude, longitude)])
+    coordinates.append((latitude, longitude))
+
+
+# Initialize Nominatim geocoder
+geolocator = Nominatim(user_agent="my_app")
+
+locations = get_address_by_cordinates(geolocator, coordinates)
+
+
+print(locations)
+
---
+++ utils.py
@@ -0,0 +1,42 @@
+from dateutil import parser
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
+
+def parse_timestamp(timestamp: str) -> dict:
+    # Parse the ISO 8601 timestamp
+    dt = parser.isoparse(timestamp)
+    
+    # Extract date and time
+    date = dt.date()
+    time = dt.time()
+    
+    # Store in dictionary
+    result = {
+        'date': date,
+        'time': time
+    }
+    
+    return result
+
+
+def get_address_by_cordinates(geolocator: Nominatim, coordinates: list[tuple]):
+
+    # List to store location results
+    location_results = []
+
+    # Iterate over coordinates and retrieve location information
+    for lat, lon in coordinates:
+        try:
+            location = geolocator.reverse(f"{lat}, {lon}")
+            location_results.append(location.address)
+        except GeocoderTimedOut:
+            print(f"Geocoding service timed out for coordinates: {lat}, {lon}")
+            location_results.append(None)  # Or handle the error as needed
+
+    return location_results
+    # Print or use the location results
+    # for result in location_results:
+    #     if result:
+    #         print(result)
+    #     else:
+    #         print("No location found")
