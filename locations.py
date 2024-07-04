b91ee7ea0fe8d139ce1c9e1985b84e9857d1aeae
ArielMoise23 <ariel.m23@gmail.com>
2024-07-04 12:33:56 -0300

    handling data with runs

--- locations.py
+++ locations.py
@@ -11,7 +11,7 @@ with open(file_name, 'r') as file:
     data = json.load(file)
 
 # # Initialize the Nominatim geocoder
-geolocator = Nominatim(user_agent="00")
+geolocator = Nominatim(user_agent="0")
 
 locations: list = []
 coordinates: list = []
@@ -22,19 +22,33 @@ for location in data['locations']:
     latitude: int = int(location['latitudeE7']) / 10**7
     longitude: int = int(location['longitudeE7']) / 10**7
 
-    time = parse_timestamp(location['timestamp'])
-    locations.append([time, (latitude, longitude)])
+    # time = parse_timestamp(location['timestamp'])
+    # locations.append([time, (latitude, longitude)])
+    locations.append([location['timestamp'], (latitude, longitude)])
     coordinates.append((latitude, longitude))
 
 
-# Initialize Nominatim geocoder
-geolocator = Nominatim(user_agent="my_app")
+## get data that already exists and cound the information to get the info beyond that
+## cant to too many place in one pull.
+try: 
+    with open('locations.json', 'r') as file:
+        existing_list = json.load(file)
+except:
+    existing_list = []
 
-addresses = get_address_by_cordinates(geolocator, coordinates[:50]) ## for testing. next step without :50
+count_of_previous_searches: int = len(existing_list) if len(existing_list) else 0
+
+addresses = get_address_by_cordinates(geolocator, coordinates[count_of_previous_searches:count_of_previous_searches+50]) ## for testing. next step without :50
 
 for i, address in enumerate(addresses):
+    i = i + count_of_previous_searches
     locations[i].append(address)
 
+with open('locations.json', 'w') as file:
+    existing_list.extend(locations[:count_of_previous_searches+50])
+    json.dump(
+        existing_list,
+        file, ensure_ascii=False, indent=4
+    )
 
-print(locations)
-
+# print(locations)
